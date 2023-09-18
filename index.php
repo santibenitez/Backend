8ecace68098a5e0eaf595b540e062ae6c4c396c4
santibenitez <110836231+santibenitez@users.noreply.github.com>
2023-09-18 18:38:42 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,50 @@
+
+<?php
+
+  echo "Hola Mundo!!";
+
+  echo "<br>";
+
+  $variable = "Hola Mundoooo!";
+    echo $variable;
+
+  echo "<br>";
+
+  $num1 = 15;
+  $num2 = 20;
+
+  echo $num1 + $num2;
+  echo "<br>";
+
+  echo $num1 - $num2;
+  echo "<br>";
+
+  echo $num1 * $num2;
+  echo "<br>";
+
+  echo $num1 / $num2;
+  echo "<br>";
+
+  echo $num1 % $num2;
+  echo "<br>";
+
+  $temperatura = 20;
+  echo "<br>";
+  echo ($temperatura * 9/5) + 20;
+  echo "<br>";
+
+  $base = 18;
+  $altura = 12;
+  echo ($base * 2)+ ($altura * 2);
+  echo "<br>";
+  echo $base * $altura;
+  echo "<br>";
+
+  $radio = 30;
+  echo 2 * $radio * 3.14;
+  echo "<br>";
+  echo 3.14 * $radio * $radio;
+
+
+
+?>
